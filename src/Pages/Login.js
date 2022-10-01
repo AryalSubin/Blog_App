6f833bd23c153cb9f6aab698ba1b94fb457eb087
Bishalsharma733 <bishalsharma733@gmail.com>
2022-10-01 23:09:49 +0545

    Register with email

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -2,6 +2,8 @@ import React from "react";
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import Register from "./Pages/Register";
+
 
 const Login = ({ setIsAuth }) => {
   const navigate = useNavigate();
@@ -18,6 +20,10 @@ const Login = ({ setIsAuth }) => {
       <p>Sign In With Google To Continue</p>
       <button className="login-with-google-btn" onClick={signInWithGoogle}>
         Sign In With Google
+      </button>
+      <p>Sign up with email</p>
+      <button className="login-with-email" onClick={Register}>
+        Register
       </button>
     </div>
   );
